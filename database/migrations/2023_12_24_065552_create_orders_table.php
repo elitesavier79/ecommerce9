ab3dfe5a72e60ba132fd5adc731b3ea1ad8414dd
Dicky Swardi <windseven79@gmail.com>
2023-12-24 14:24:08 +0700

    buat migration database

--- database/migrations/2023_12_24_065552_create_orders_table.php
+++ database/migrations/2023_12_24_065552_create_orders_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->boolean('is_admin')->default(false);
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->boolean('is_paid')->default(false);
+            $table->string('payment_receipt');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
